f61f0072e51b5756b20acb4203a87bae935b6777
a7mad1112 <ahmalawneh79@gmail.com>
2026-09-21 23:09:30 +0300

    test: add city management unit tests

---
+++ HotelBooking.Tests/Unit/Features/Cities/GetCities/GetCitiesServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using HotelBooking.Application.Features.Cities;
+using HotelBooking.Application.Features.Cities.GetCities;
+using HotelBooking.Domain.Entities;
+using Moq;
+
+namespace HotelBooking.Tests.Unit.Features.Cities.GetCities;
+
+public class GetCitiesServiceTests
+{
+    [Fact]
+    public async Task GetAllAsync_ReturnsCities()
+    {
+        var repository = new Mock<ICitiesRepository>();
+
+        repository
+            .Setup(x => x.GetAllAsync(
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(
+            [
+                new City
+                {
+                    Id = 1,
+                    Name = "Amman",
+                    Country = "Jordan"
+                }
+            ]);
+
+
+        var service = new GetCitiesService(
+            repository.Object);
+
+
+        var result = await service.GetAllAsync(
+            CancellationToken.None);
+
+
+        Assert.Single(result);
+
+        Assert.Equal(
+            "Amman",
+            result[0].Name);
+
+        Assert.Equal(
+            "Jordan",
+            result[0].Country);
+    }
+}
